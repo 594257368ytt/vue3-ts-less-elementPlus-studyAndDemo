0f77aab51d71f45fbdc8d4a1d0723e30162cbaf5
姚婷婷 <yaott@xiaopeng.com>
2023-01-12 18:02:40 +0800

    fix: vue3引入，feat: 知识点1

--- vuex/src/App.vue
+++ vuex/src/App.vue
@@ -1,14 +1,14 @@
 <template>
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Q1 msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Q1 from './components/question1.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Q1
   }
 }
 </script>
